5a0b70f8f8d2f5d1fcc86b6fab908698a59a961f
limochen <2320035651@qq.com>
2022-10-21 15:18:45 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>hi</div>
+ <Limo />
+</template>
+
+<script>
+import Limo from './components/Limo.vue'
+
+export default {
+  name: 'App',
+  components: {
+  Limo
+  }
+}
+</script>
