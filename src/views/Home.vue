97cabf5ffcbca1148b6aef9e8f079bcae1b07edb
limochen <2320035651@qq.com>
2022-10-22 13:18:48 +0800

    topnav样式更新好了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>轮子UI</h1>
             <h2>一个厉害的UI框架</h2>
             <p class="actions">
-                <a>Github</a>
-                <a href="">开始</a>
+                <a href="https://github.com">GitHub</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
@@ -17,5 +17,27 @@ export default{
     components:{Topnav}
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
 
